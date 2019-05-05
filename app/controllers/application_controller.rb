25ff18b0e89dfd702c62e473bf8a6921fba46491
Takagi <uruhuodoneru2002@gmail.com>
2019-05-05 15:18:47 +0000

    ビュー記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,20 @@
 class ApplicationController < ActionController::Base
 
 	before_action :configure_permitted_parameters, if: :devise_controller?
-	before_action :authenticate_user!
+
+	before_action :login_require
+
+	def login_require
+	    redirect_to login_path unless current_user
+	end
+
 
 	def after_sign_in_path_for(resource)
-		mypage_root_path
+		root_path
 	end
 
 	def after_sign_up_path_for(resource)
-		mypage_root_path
+		root_path
 	end
 
 	protected
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,15 +1,53 @@
 class SongsController < ApplicationController
 
+
+    def index
+    	@artists = Artist.all.order(created_at: :desc)
+    end
  	def new
+ 		@artist = Artist.new
     end
 
     def create
+    	@artist = Artist.new(artist_params)
+    	if @artist.save
+    		redirect_to new_song_path, notice: アーティスト作成に成功しました。
+    	else
+    		redirect_to new_artist_path, notice: アーティストの作成に失敗しました。
+    	end
+
     end
 
-    def index
-    end
 
     def show
+    	@artist = Artist.find(params[:id])
+    end
+
+    def edit
+    	@artist = Artist.find(params[:id])
+    end
+
+    def update
+    	@artist = artist.find(params[:id])
+    	if @artist.update(artist_params)
+    		redirect_to artist_path(@artist.id), notice: 'アーティスト名が編集されました。'
+    	else
+    		render :index, notice: 'アーティストの削除に失敗。'
+    	end
+    end
+
+    def destroy
+    	artist = Artist.find(params[:id])
+    	if artist.delete
+    	redirect_to artists_path, notice: 'アーティストは削除されました。'
+    	else
+    		render :index, notice: 'アーティストの削除に失敗。'
+    	end
+    end
+
+    private
+    def artist_params
+    	params.require(:artist).permit(:artist_name)
     end
 
 end
