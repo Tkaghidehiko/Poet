32216c841536d7f53566315e482361164213334a
Takagi <uruhuodoneru2002@gmail.com>
2019-05-06 09:25:31 +0000

    modelアソシエーションthrough追記

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,6 @@
 class Artist < ApplicationRecord
-	has_many :songs, dependent: :destroy
 	belongs_to :user
+	has_many :songs, dependent: :destroy
+	has_many :post_poems, through: :songs, dependent: :destroy
+  	has_many :post_trans, through: :post_poems, dependent: :destroy
 end
