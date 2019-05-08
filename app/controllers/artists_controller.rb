e301ab4b63d86817b46a833290f777623691442a
Takagi <uruhuodoneru2002@gmail.com>
2019-05-08 11:52:52 +0000

    待て待て

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -4,51 +4,58 @@ class ArtistsController < ApplicationController
     def index
     	@artists = Artist.all.order(created_at: :desc)
     end
- 	def new
- 		@artist = Artist.new
+
+    def new
+        @artist = Artist.new
+        @song = @artist.songs.build
+        @poem = @song.post_poems.build
     end
 
     def create
-    	@artist = Artist.new(artist_params)
-    	# if
-        @artist.save
-    	redirect_to new_song_path(@artist), notice: "アーティスト作成に成功しました。"
-    	# else
-    	# 	redirect_to new_artist_path, notice: "アーティストの作成に失敗しました。"
-    	# end
-
+        @artist= Artist.new(artist_params)
+        if @artist.save!
+         redirect_to song_path(@song.id)
+        else
+          @song = @srtist.songs.build
+          @poem = @song.post_poems.build
+          render :new
+        end
     end
+
+
 
 
     def show
-    	@artist = Artist.find(params[:id])
+        @artist = Artist.find(params[:id])
     end
 
     def edit
-    	@artist = Artist.find(params[:id])
+        @artist = Artist.find(params[:id])
     end
 
     def update
-    	@artist = Atist.find(params[:id])
-    	if @artist.update(artist_params)
-    		redirect_to artist_path(@artist.id), notice: 'アーティスト名が編集されました。'
-    	else
-    		render :index, notice: 'アーティストの編集に失敗。'
-    	end
+        @artist = Atist.find(params[:id])
+        @artist.user_id = current_user.id
+        if @artist.update(artist_params)
+            redirect_to artist_path(@artist.id), notice: 'アーティスト名が編集されました。'
+        else
+            render :index, notice: 'アーティストの編集に失敗。'
+        end
     end
 
     def destroy
-    	artist = Artist.find(params[:id])
-    	if artist.delete
-    	redirect_to artists_path, notice: 'アーティストは削除されました。'
-    	else
-    		render :index, notice: 'アーティストの削除に失敗。'
-    	end
+        artist = Artist.find(params[:id])
+        if artist.delete
+        redirect_to artists_path, notice: 'アーティストは削除されました。'
+        else
+            render :index, notice: 'アーティストの削除に失敗。'
+        end
     end
 
     private
     def artist_params
-    	params.require(:artist).permit(:artist_name)
+        params.require(:artist).permit(:artist_name,
+                                        songs_attributes: [:id, :album_name, :song_name, :_destroy,
+                                        post_poems_attributes: [:id, :poem, :_destroy]])
     end
-
 end
