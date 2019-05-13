3098a00eb9662e16d74fba7019c51dbaa19e7071
Takagi <uruhuodoneru2002@gmail.com>
2019-05-13 15:12:26 +0000

    githubバグ修正

--- db/migrate/20190501085804_create_post_trans.rb
+++ db/migrate/20190501085804_create_post_trans.rb
@@ -2,8 +2,8 @@ class CreatePostTrans < ActiveRecord::Migration[5.2]
   def change
     create_table :post_trans do |t|
       t.text :song_translate
-      t.references :post_poem_id, foreign_key: true
-      t.references :user_id, foreign_key: true
+      t.references :post_poem, foreign_key: true
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20190501085930_create_favorites.rb
+++ db/migrate/20190501085930_create_favorites.rb
@@ -1,9 +1,9 @@
 class CreateFavorites < ActiveRecord::Migration[5.2]
   def change
     create_table :favorites do |t|
-      t.references :song_id, foreign_key: true
-      t.references :post_tran_id, foreign_key: true
-      t.references :user_id, foreign_key: true
+      t.references :song, foreign_key: true
+      t.references :post_tran, foreign_key: true
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
